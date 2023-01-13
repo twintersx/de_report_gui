ddffa4a60103f44e50f547810b84b0cc7b1869c2
twintersx <twintersx@gmail.com>
2023-01-13 14:00:54 -0800

    save stream as gif and add to fol webpage

--- stream-recorder.py
+++ stream-recorder.py
@@ -1,31 +1,32 @@
 import cv2
 from datetime import datetime, timedelta
+import imageio 
 
 cap = cv2.VideoCapture(0)   # change to 1 for input
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
 t0 = datetime.now()
-tf = t0 + timedelta(seconds=5)
+tf = t0 + timedelta(seconds=10)
 dt_string = t0.strftime("%Y%m%d_%H%M%S")
-out = cv2.VideoWriter(f'{dt_string}.avi', fourcc, 20.0, (640, 480))
 
+frames = []
 while(datetime.now() < tf):
     # ret checks return at each frame
     ret, frame = cap.read()
 
-    # output the frame
-    out.write(frame)
+    # --- save frames for later --- #
+    frames.append(frame)
 
     # The original input frame is shown in the window 
     cv2.imshow('Original', frame)
 
-    # Wait for 'q' key to stop the program 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # pressing 'a' simulates a new DE during recording 
+    if cv2.waitKey(1) & 0xFF == ord('a'):
+        tf = tf + timedelta(seconds=10)
 
+with imageio.get_writer(f'{dt_string}.gif', mode='I', fps = 5) as writer:
+    for i in range(0, len(frames), 10):
+        rgb_frame = cv2.cvtColor(frames[i], cv2.COLOR_BGR2RGB)
+        writer.append_data(rgb_frame)
 
 cap.release()               # Close the window / Release webcam
-out.release()               # After we release our webcam, we also release the output
 cv2.destroyAllWindows()     # De-allocate any associated memory usage
