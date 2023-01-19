cbd96aa334ca24ce6fe112a67026703e02be0db3
twintersx <twintersx@gmail.com>
2023-01-19 09:32:53 -0800

    working pre js save to localStorage

--- pyqt5_testing.py
+++ pyqt5_testing.py
@@ -11,10 +11,12 @@ from PyQt5 import QtCore
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()  # referencing the "parent" or "super" class, QWidget which the child class MyApp needs.
+        self.initUI()
+    
+    def initUI(self):
         self.setWindowTitle('Disengagment Report Map')
         self.window_width, self.window_height = 1600, 1200
         self.setMinimumSize(self.window_width, self.window_height)
-
         layout = QVBoxLayout()
         self.setLayout(layout)
 
@@ -25,32 +27,32 @@ class MyApp(QWidget):
         encoded = base64.b64encode(open(img, 'rb').read()).decode()
         gif_html = f'<img src="data:image/gif;base64,{encoded}">'
 
-        html = f"""{gif_html}
-                    <br>DESCRIPTION OF FACTS CAUSING DISENGAGEMENT: <input type="text" value="{"The AV steered offcourse"}" id="myInput"><br> 
-                    <button onclick="myFunction()">Save</button>"""
+        html = f"""{gif_html}<br><br>
+                    DESCRIPTION OF FACTS CAUSING DISENGAGEMENT: <input type="text" value="{"The AV steered offcourse"}" id="myInput" size="125"><br><br>
+                    PLEASE SELECT A ROAD TYPE: 
+                    <input type="radio" id="street" name="road_type" value="Street"> <label for="street">Street</label>
+                    <input type="radio" id="highway" name="road_type" value="Highway"> <label for="highway">Highway</label><br>
+                    <button onclick="myFunction()">Save</button>
+                """
             
         folium.Marker(location=[37.391200, -122.001061], popup=html).add_to(m)
-
+        
         java = folium.MacroElement().add_to(m)
         java._template = jinja2.Template("""
             {% macro script(this, kwargs) %}
             function myFunction() {
                 var copyText = document.getElementById("myInput");
                 copyText.select();
-                //navigator.clipboard.writeText(copyText.value); //does not work with qwebengineview()
-                document.execCommand('copy');
+                document.execCommand('copy')
             }
             {% endmacro %}
         """)
 
         m.save('map.html')
-
         url = QtCore.QUrl.fromLocalFile(os.path.join(os.getcwd(), 'map.html'))
-
         webView = QWebEngineView()  # used to view and edit web documents
         webView.load(url)   
         layout.addWidget(webView)
-
 
 app = QApplication(sys.argv)
 app.setStyleSheet('QWidget {font-size: 35px;}')
