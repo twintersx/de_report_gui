891226bf2977080de2f09ec848ae952d17d1d4a0
twintersx <twintersx@gmail.com>
2023-01-24 18:04:20 -0800

    map is updating

--- gui_noMap.py
+++ gui_noMap.py
@@ -12,6 +12,7 @@ class MainApplication(tk.Frame):
         self.initFrames()
         self.initWidgets()
         self.setDateReport()
+        self.setMap()
 
     def initFrames(self):        
         self.controlFrame = tk.Frame(self.parent, width=120, highlightbackground="grey", highlightthickness=2)
@@ -24,28 +25,50 @@ class MainApplication(tk.Frame):
         self.cal = Calendar(self.controlFrame, selectmode='day', year=today.year, month=today.month, day=today.day)
         self.cal.pack(side=tk.TOP, padx=self.pad, pady=self.pad)
 
-        dateButton = tk.Button(self.controlFrame, text="Change Date", command=self.setDateReport)
+        dateButton = tk.Button(self.controlFrame, text="Change Date", command=self.onDateChangeClick)
         dateButton.pack(fill=tk.X, side=tk.TOP, padx=self.pad, pady=self.pad)
+
+    def onDateChangeClick(self):
+        self.setDateReport()
+        self.changeMapPosition()
 
     def setDateReport(self):
         map_date = datetime.strptime(self.cal.get_date(), '%m/%d/%y')
 
-        report = []
+        self.report = []
         with open('de_reports.csv', newline='') as csvfile:
             reader = list(csv.reader(csvfile, delimiter=','))
-            date_index = reader[0].index("DATE")
+            self.header = reader[0]
+            date_index = self.header.index("DATE")
             for row in reader[1:]:
                 report_date = datetime.strptime(row[date_index], '%d/%m/%Y')
                 if report_date >= map_date:
-                    report.append(row)
+                    self.report.append(row)
 
-        print(report)
+    def changeMapPosition(self):
+        lat_index = self.header.index("LATITUDE")
+        long_index = self.header.index("LONGITUDE")
 
+        # at least one report
+        if len(self.report) >= 1:
+            lats, longs = [], []
+            for row in self.report:
+                lats.append(float(row[lat_index]))
+                longs.append(float(row[long_index]))
+            latPos = sum(lats) / len(lats)
+            longPos = sum(longs) / len(longs)
 
-    def initMap(self):
-        # Map
-        map_widget = TkinterMapView(self.mapFrame, width=500, height=500)
-        map_widget.pack()
+        # no reports, set to lab center
+        elif len(self.report) == 0:
+            latPos, longPos = 37.376774, -121.989967
+
+        self.map_widget.set_position(latPos, longPos)
+        self.map_widget.set_zoom(14)
+            
+    def setMap(self):
+        self.map_widget = TkinterMapView(self.mapFrame, width=500, height=500)
+        self.changeMapPosition()
+        self.map_widget.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
